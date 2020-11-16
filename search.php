0adc1b9572baf8f8d10e44c8b910f203d17c8587
Sofiane <Sofiane.Benadjila@hotmail.fr>
2020-11-16 15:29:22 +0100

    Ajout du template de restultat de recherche
    Presque parfait reste a trouver comment afficher le terme de recherche

---
+++ search.php
@@ -0,0 +1,11 @@
+<?php
+    get_header();
+?>
+    <div class="category">
+        <h1>terme de recherche XXXXXXXX</h1>
+    <?php get_template_part('loop'); ?>
+    </div>
+        <?php
+            get_sidebar();
+            get_footer();
+?>
